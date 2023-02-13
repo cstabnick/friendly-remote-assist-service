a0255d777e4777c1d690964f4dce165fc455a067
Connor Stabnick <cstabnick@gmail.com>
2023-02-12 20:22:31 -0500

    Initial commit for remote assist service

---
+++ FriendlyRemoteAssistantService/BackgroundServices/MyService.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.WebSockets;
+
+namespace FriendlyRemoteAssistantService.BackgroundServices;
+
+public class MyService : BackgroundService
+{
+    private WebSocket _wsSender = null;
+    private WebSocket _wsReceiver = null;
+
+    private int _messageSize = 4096;
+
+    public void SetSender(WebSocket ws)
+    {
+        _wsSender = ws;
+    }
+
+    public void SetReceiver(WebSocket ws)
+    {
+        _wsReceiver = ws;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Queue<byte[]> messageQueue = new Queue<byte[]>();
+        while (true)
+        {
+            if (_wsSender != null && _wsReceiver != null)
+            {
+                List<byte> incomingMessage = new List<byte>();
+
+                Task.Run(() =>
+                {
+                    byte[] sendBuffer = new byte[_messageSize];
+                    while (true)
+                    {
+                        var result = _wsSender.ReceiveAsync(new ArraySegment<byte>(sendBuffer), stoppingToken).GetAwaiter().GetResult();
+
+                        incomingMessage.AddRange(sendBuffer);
+                        if (result.EndOfMessage)
+                        {
+                            messageQueue.Enqueue(incomingMessage.ToArray());
+                            incomingMessage.Clear();
+                        }
+                    }
+                });
+
+                await Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        if (messageQueue.Count > 0)
+                        {
+                            byte[] ba = messageQueue.Dequeue();
+
+                            byte[] sendBuffer = new byte[_messageSize];
+                            using (var ms = new MemoryStream(ba, false))
+                            {
+                                for (int i = 0; ba.Length > _messageSize * i; i++)
+                                {
+                                    await ms.ReadAsync(sendBuffer, 0, _messageSize);
+
+                                    var lastMessage = ba.Length <= _messageSize * (i + 1);
+                                    await _wsReceiver.SendAsync(new ArraySegment<byte>(sendBuffer),
+                                        WebSocketMessageType.Binary, lastMessage,
+                                        CancellationToken.None);
+                                }
+                            }
+                        }
+                        else
+                            await Task.Delay(500);
+                    }
+                });
+            }
+            else
+            {
+                Console.WriteLine("Not yet connected");
+                await Task.Delay(500);
+            }
+        }
+
+        return;
+    }
+}
---
+++ FriendlyRemoteAssistantService/Controllers/WebSocketController.cs
@@ -0,0 +1,102 @@
+﻿using System.Net.WebSockets;
+using FriendlyRemoteAssistantService.BackgroundServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendlyRemoteAssistantService.Controllers;
+
+[ApiController]
+public class WebSocketController : ControllerBase
+{
+    private MyService _myService;
+    public WebSocketController(MyService myService)
+    {
+        _myService = myService;
+    }
+    private static async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var receiveResult = await webSocket.ReceiveAsync(
+            new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!receiveResult.CloseStatus.HasValue)
+        {
+            await webSocket.SendAsync(
+                new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                receiveResult.MessageType,
+                receiveResult.EndOfMessage,
+                CancellationToken.None);
+
+            receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+            
+            Console.WriteLine(buffer.Length);
+        }
+
+        await webSocket.CloseAsync(
+            receiveResult.CloseStatus.Value,
+            receiveResult.CloseStatusDescription,
+            CancellationToken.None);
+    }
+    
+    [HttpGet("/ws")]
+    public async Task<IActionResult> Get()
+    {
+        
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+    
+            string test = "this is a test";
+            var buffer = new byte[test.Length];
+            for (int i = 0; i < test.Length; i++)
+            {
+                buffer[i] = (byte)test[i];
+            }
+            
+                
+            await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            while (true)
+                await Echo(webSocket);
+            // webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);}
+        }
+    
+        return Ok();
+    }
+    
+    [HttpGet("/sender")]
+    public async Task<IActionResult> Sender()
+    {
+        
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+    
+            _myService.SetSender(webSocket);
+            while (true) {}
+
+            return Ok();
+            // webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);}
+        }
+    
+        return Ok();
+    }
+    
+    [HttpGet("/receiver")]
+    public async Task<IActionResult> Receiver()
+    {
+        
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+    
+            _myService.SetReceiver(webSocket);
+            while (true) {}
+            
+            return Ok();
+            // webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);}
+        }
+    
+        return Ok();
+    }
+}
